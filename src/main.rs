c003d6b81c03689c01d22904bdf5b4912c4a9923
Callum Oakley <c.oakley108@gmail.com>
2019-01-28 22:09:57 +0000

    use serde_json's value and migrate to 2018 edition

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,17 @@
-extern crate json5;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-
-use std::collections::HashMap;
+use json5;
+use serde_json::Value;
 use std::error::Error;
 use std::io::{self, Read};
 use std::process;
-
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-#[serde(untagged)]
-enum Value {
-    Null,
-    Bool(bool),
-    Number(f64),
-    String(String),
-    Array(Vec<Value>),
-    Object(HashMap<String, Value>),
-}
 
 fn run() -> Result<String, Box<Error>> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
-    let value = json5::from_str::<Value>(&buffer)?;
-
-    let s = json5::to_string(&value)?;
-
-    Ok(s)
+    Ok(json5::from_str::<Value>(&buffer)?.to_string())
 }
 
-fn main() -> () {
+fn main() {
     match run() {
         Ok(s) => println!("{}", s),
         Err(err) => {
